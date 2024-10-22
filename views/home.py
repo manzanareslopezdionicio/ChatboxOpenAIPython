8a6f4c3fd813a77b88ffc852d3a6de25c44ccb78
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-10-22 11:54:51 -0600

    webinicio

--- views/home.py
+++ views/home.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.header("Pagina principal")
+#configuracion
+#st.set_page_config(layout='wide')
+
+with st.container():
+    st.subheader("Bienvenidos, Somos SOFTIA :wave:")
+    st.title("Creamos soluciones para acelerar tu negocio")
+    st.write(
+        "Somos unos apasionados de las tecnologia y la innovación, especializados en el sector de la digitalización y automatización de negocios. Nos gusta crear soluciones para resolver problemas y mejorar procesos."
+    )
+st.write("[Saber más >](https://valerapp.com/)")
+
+# Sobre nosotros
+with st.container():
+    st.write("---")
