567a36b346b78f401ce8f8c8e64561cffcaf1c38
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2024-10-24 16:05:43 -0600

    home

--- views/acerca_de.py
+++ views/acerca_de.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
-st.title("Acerca de mi")
-st.button("enviar")
+@st.dialog("contacto") #.experimental_dialog("Contacto")
+def form_contacto():
+    st.text_input("Email", placeholder="nombre@email.com")
+    st.text_input("Nombre", placeholder="Escribir Nombres")
+    st.text_input("Apellidos", placeholder="Escribir Apellidos")
+    st.write("---")
+    st.button("Enviar", use_container_width=True)
+
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image("img/dioni.png", width=230)
+
+with  col2:
+    st.title("Dionicio Manzanares", anchor=False)
+    st.write(
+        "Analista de datos senior, que ayuda a las empresas apoyando la toma de decisiones basada en datos. Como Ciencia de Datos."
+    )
+    if st.button("Contacto"):
+        form_contacto()
--- views/home.py
+++ views/home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 #configuracion
+
 #st.set_page_config(layout='wide')
 
 with st.container():
@@ -9,8 +10,80 @@ with st.container():
     st.write(
         "Somos unos apasionados de las tecnologia y la innovación, especializados en el sector de la digitalización y automatización de negocios. Nos gusta crear soluciones para resolver problemas y mejorar procesos."
     )
-st.write("[Saber más >](https://valerapp.com/)")
+st.write("[Saber más >](https://streamlit.io/)")
 
 # Sobre nosotros
 with st.container():
     st.write("---")
+    I_columna, R_columna = st.columns((2))
+    with I_columna:
+        st.header("Sobre nosotros 🔍")
+        st.write(
+            """
+            Nuestro objetivo es poder aportar valor a los negocios ayudandoles a ahorrar tiempo y dinero a través de la implantación de nuevas tecnologías como la inteligencia artifical, analisis de datos o implantación de software de automatización.
+            Seguramente te vamos a poder ayudar si:
+
+            - Tienes un negocio y quieres mejorar tus procesos de trabajo para ahorrar tiempo y dinero
+            - Tienes trabajadores que emplean parte de su jornada a realizar tareas repetitivas sin valor añadido para tu negocio
+            - No tienes claras las métricas de tu negocio y quieres tomar decisiones basadas en datos
+            - Quieres mejorar la experiencia de tus clientes
+            - Usas herramientas de software antiguas o poco eficientes o procesos en los que usas papel y bolígrafo
+
+            ***Si esto suena interesante para ti puedes contactarnos a través del formulario que encontrarás al final de la página*** 
+            """
+        )
+        st.write("[Más sobre nosotros >](https://streamlit.io/)")
+    with R_columna:
+        st.empty()
+    
+    #Servicios
+    with st.container():
+        st.write("---")
+        st.header("Servicios")
+        #st.write("##")
+        imagen_columna, texto_columna = st.columns((1,2))
+        with imagen_columna:
+            st.image("img/chatbot.png")
+        
+        with texto_columna:
+            st.subheader("Diseño de aplicaciones")
+            st.write(
+                """
+                Si en tus procesos diarios tienes que introducir información en diferentes fuentes de datos o bien tienes que trabajar con documentación en papel, es hora de pensar en implementar una aplicación en tu negocio para potenciar y optimizar el funcionamiento de los procesos diarios.
+                """
+            )
+            st.write("[Ver servicios > ](https://streamlit.io/)")
+    
+    with st.container():
+        st.write("---")
+        #st.write("##")
+        imagen_columna, texto_columna = st.columns((1,2))
+        with imagen_columna:
+            st.image("img/chatbot.png")
+        
+        with texto_columna:
+            st.subheader("Automatización de procesos")
+            st.write(
+                """
+                Si realizas cualquier tipo de tarea repetitiva como por ejemplo introducir datos en excel u otras aplicaciones, gestión de facturas, envío de emails a proveedores etc Lo que quizás necesitas es una automatización de tareas para poder liberar recursos de esas actividades y poder emplearlos en otras tareas más productivas.
+                """
+            )
+            st.write("[Ver servicios > ](https://streamlit.io/)")
+
+    with st.container():
+        st.write("---")
+        #st.write("##")
+        imagen_columna, texto_columna = st.columns((1,2))
+        with imagen_columna:
+            st.image("img/chatbot.png")
+        
+        with texto_columna:
+            st.subheader("Visualización de datos")
+            st.write(
+                """
+                Si sientes que no tienes una visión clara de datos de tu negocio lo que necesitas es una aplicación en la que puedas tener toda la información de interes de tu empresa.
+                """
+            )
+            st.write("[Ver servicios > ](https://streamlit.io/)")
+
+        
